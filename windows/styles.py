e540bbb2bf4f20af35ff6d488db23fde15cbbee7
MaffyMaths <matvey33.baryshev@mail.ru>
2023-04-22 10:27:43 +0300

    add styles.py file

---
+++ windows/styles.py
@@ -0,0 +1,10 @@
+DEFAULT_LINE_STYLE = "color: rgb(0, 0, 0);\n" \
+                     "background-color: rgb(255, 255, 255);\n" \
+                     "border-radius: 15px;\n" \
+                     "border-radius: 15px; \n" \
+                     "border: 2px solid #000000;\n" \
+                     "padding-left: 20px\n"
+DEFAULT_TITLE_STYLE = "color: rgb(0, 0, 0);"
+BLACK = "#000000"
+WHITE = "#FFFFFF"
+RED = "#ff0200"
