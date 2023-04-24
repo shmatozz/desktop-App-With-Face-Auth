fb6a5cd7d36296a4ebc7fe549e644dd6fde87da6
MaffyMaths <matvey33.baryshev@mail.ru>
2023-04-24 17:54:37 +0300

    add "Are you sure?" dialog window

---
+++ ui/confirm_dialog.py
@@ -0,0 +1,101 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QDialog
+
+
+class UiConfirm(QDialog):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.setWindowModality(QtCore.Qt.WindowModality.NonModal)
+        Dialog.resize(400, 200)
+        Dialog.setMinimumSize(QtCore.QSize(400, 200))
+        Dialog.setMaximumSize(QtCore.QSize(400, 200))
+        Dialog.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.NoContextMenu)
+        Dialog.setSizeGripEnabled(False)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Dialog)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.dialog_frame = QtWidgets.QFrame(parent=Dialog)
+        self.dialog_frame.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.dialog_frame.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.dialog_frame.setObjectName("dialog_frame")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.dialog_frame)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.question_laber = QtWidgets.QLabel(parent=self.dialog_frame)
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift")
+        font.setPointSize(24)
+        self.question_laber.setFont(font)
+        self.question_laber.setObjectName("question_laber")
+        self.verticalLayout.addWidget(self.question_laber, 0, QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.frame_2 = QtWidgets.QFrame(parent=self.dialog_frame)
+        self.frame_2.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.frame_2)
+        self.horizontalLayout_2.setSpacing(40)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.ok_button = QtWidgets.QPushButton(parent=self.frame_2)
+        self.ok_button.setMaximumSize(QtCore.QSize(200, 50))
+        self.ok_button.setStyleSheet("QPushButton {\n"
+                                     "    font: 75 14pt \"Bahnschrift\";\n"
+                                     "    color: rgb(255, 255, 255);\n"
+                                     "    background-color: rgb(120, 183, 140);\n"
+                                     "    border-radius: 10px;\n"
+                                     "    border: 2px solid #094065;\n"
+                                     "}\n"
+                                     "\n"
+                                     "QPushButton:hover{\n"
+                                     "    font: 75 14pt \"Bahnschrift\";\n"
+                                     "    color: rgb(255, 255, 255);\n"
+                                     "    background-color: rgb(100, 183, 140);\n"
+                                     "    border-radius: 10px;\n"
+                                     "    border: 2px solid #094065;\n"
+                                     "}\n"
+                                     "\n"
+                                     "QPushButton:pressed{\n"
+                                     "    font: 75 14pt \"Bahnschrift\";\n"
+                                     "    color: rgb(255, 255, 255);\n"
+                                     "    background-color: rgb(88, 162, 123);\n"
+                                     "    border-radius: 10px;\n"
+                                     "    border: 2px solid #094065;\n"
+                                     "}")
+        self.ok_button.setObjectName("ok_button")
+        self.horizontalLayout_2.addWidget(self.ok_button)
+        self.cancel_button = QtWidgets.QPushButton(parent=self.frame_2)
+        self.cancel_button.setMaximumSize(QtCore.QSize(200, 50))
+        self.cancel_button.setStyleSheet("QPushButton {\n"
+                                         "    font: 75 14pt \"Bahnschrift\";\n"
+                                         "    color: rgb(255, 0, 0);\n"
+                                         "    background-color: rgb(255, 255, 255);\n"
+                                         "    border-radius: 10px;                     /* <----  20px  */ \n"
+                                         "    border: 2px solid #094065;\n"
+                                         "}\n"
+                                         "\n"
+                                         "QPushButton:hover{\n"
+                                         "    font: 75 14pt \"Bahnschrift\";\n"
+                                         "    color: rgb(255, 255, 255);\n"
+                                         "    background-color: rgb(234, 0, 0);\n"
+                                         "    border-radius: 10px;                     /* <----  20px  */ \n"
+                                         "    border: 2px solid #094065;\n"
+                                         "}\n"
+                                         "\n"
+                                         "QPushButton:pressed{\n"
+                                         "    font: 75 14pt \"Bahnschrift\";\n"
+                                         "    color: rgb(255, 255, 255);\n"
+                                         "    background-color:rgb(182, 0, 0);\n"
+                                         "    border-radius: 10px;                     /* <----  20px  */ \n"
+                                         "    border: 2px solid #094065;\n"
+                                         "}")
+        self.cancel_button.setObjectName("cancel_button")
+        self.horizontalLayout_2.addWidget(self.cancel_button)
+        self.verticalLayout.addWidget(self.frame_2)
+        self.horizontalLayout.addWidget(self.dialog_frame)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.question_laber.setText(_translate("Dialog", "Are you sure?"))
+        self.ok_button.setText(_translate("Dialog", "OK"))
+        self.cancel_button.setText(_translate("Dialog", "Cancel"))
