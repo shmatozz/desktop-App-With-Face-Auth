e24fd709b0534fec59916b0acf8a3df81eb41723
MaffyMaths <matvey33.baryshev@mail.ru>
2023-04-25 19:05:59 +0300

    add face auth to sign in menu

--- windows/sign_in_window.py
+++ windows/sign_in_window.py
@@ -1,9 +1,13 @@
 import psycopg2
+import cv2
+from torch import dist
+from PIL import Image
 from PyQt6.QtWidgets import QMainWindow
 from ui.signInMenu import UiSignIn
 from windows.sign_up_window import SignUp
 from windows.main_window import MainWindow
 from windows.styles import *
+from facenet_pytorch import MTCNN, InceptionResnetV1
 
 
 class SignIn(QMainWindow):
@@ -33,7 +37,43 @@ class SignIn(QMainWindow):
             self.ui.helloTitle.setText("Missing required fields!")
             self.ui.password.setStyleSheet(DEFAULT_LINE_STYLE.replace(BLACK, RED))
 
-        if len(login) != 0 and len(password) != 0:
+        # TODO refactor sign in procedure
+        try:
+            connection = self.connect_to_db()
+            with connection.cursor() as cursor:
+                cursor.execute(f"select password, enable_face_auth, user_face from users where login = '{login}'")
+                info = cursor.fetchone()    # info[0] = password, info[1] = True/False face auth, info[2] = binary photo
+            connection.close()
+        except psycopg2.Error:
+            print("[INFO] database error")
+
+        if len(login) != 0 and info[1]:
+            with open("user_data/face_photo.png", "wb") as photo:
+                photo.write(info[2])
+            mtcnn = MTCNN(image_size=500, margin=0, min_face_size=200)  # initializing mtcnn for face detection
+            resnet = InceptionResnetV1(pretrained='vggface2').eval()
+            face_database = mtcnn(Image.open("user_data/face_photo.png"))
+            emb_database = resnet(face_database.unsqueeze(0)).detach()
+            cam = cv2.VideoCapture(0)
+            count_trys = 0
+            count_success = 0
+            while count_trys < 2:
+                result, image = cam.read()
+                cv2.imwrite("user_data/face_photo.png", image)
+                face_current = mtcnn(Image.open("user_data/face_photo.png"))
+                emb_current = resnet(face_current.unsqueeze(0)).detach()
+                distance = dist(emb_current, emb_database).item()
+                print(distance)
+                if distance < 0.6:
+                    count_success += 1
+                count_trys += 1
+            if count_success > 0:
+                self.close()
+                self.main = MainWindow(login)
+                self.main.show()
+            else:
+                self.ui.helloTitle.setText("Error, try again or sign in with password")
+        elif len(login) != 0 and len(password) != 0:
             try:
                 # connect to database
                 connection = self.connect_to_db()
@@ -48,10 +88,15 @@ class SignIn(QMainWindow):
                         self.ui.login.setStyleSheet(DEFAULT_LINE_STYLE.replace(BLACK, RED))
                     # if database password == input password
                     elif user_password[0] == password:
+                        with open("user_data/data.py", "w") as datafile:
+                            datafile.seek(0)
+                            datafile.write(f"logged = True\nlogin = '{login}'\npassword = '{password}'\n")
+                            datafile.truncate()
+
                         self.ui.helloTitle.setText("PASSED!")
                         self.ui.helloTitle.setStyleSheet("color: rgb(120, 183, 140);")
                         self.close()
-                        self.main = MainWindow(login, password)
+                        self.main = MainWindow(login)
                         self.main.show()
                     # if database password != input password
                     else:
