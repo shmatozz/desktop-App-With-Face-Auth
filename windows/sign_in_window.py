207e477da7b012b753545a8461a13887d2457623
MaffyMaths <matvey33.baryshev@mail.ru>
2023-04-26 16:27:23 +0300

    refactor sign in procedure with face auth

--- windows/sign_in_window.py
+++ windows/sign_in_window.py
@@ -17,6 +17,7 @@ class SignIn(QMainWindow):
         self.main = None
         self.ui = UiSignIn()
         self.ui.setupUi(self)
+        self.cam = cv2.VideoCapture(0)
 
         self.ui.SignInButton.clicked.connect(self.signIn)
         self.ui.SignUpButton.clicked.connect(self.openSignUp)
@@ -37,7 +38,6 @@ class SignIn(QMainWindow):
             self.ui.helloTitle.setText("Missing required fields!")
             self.ui.password.setStyleSheet(DEFAULT_LINE_STYLE.replace(BLACK, RED))
 
-        # TODO refactor sign in procedure
         try:
             connection = self.connect_to_db()
             with connection.cursor() as cursor:
@@ -47,65 +47,57 @@ class SignIn(QMainWindow):
         except psycopg2.Error:
             print("[INFO] database error")
 
-        if len(login) != 0 and info[1]:
+        if len(login) != 0 and len(password) != 0:
+            if info is None:
+                self.ui.helloTitle.setText("Wrong login!")
+                self.ui.login.setStyleSheet(DEFAULT_LINE_STYLE.replace(BLACK, RED))
+            # if database password == input password
+            elif info[0] == password:
+                with open("user_data/data.py", "w") as datafile:
+                    datafile.seek(0)
+                    datafile.write(f"logged = True\nlogin = '{login}'\npassword = '{password}'\n")
+                    datafile.truncate()
+
+                self.ui.helloTitle.setText("PASSED!")
+                self.ui.helloTitle.setStyleSheet("color: rgb(120, 183, 140);")
+                self.close()
+                self.main = MainWindow(login)
+                self.main.show()
+            # if database password != input password
+            else:
+                self.ui.helloTitle.setText("Wrong password!")
+                self.ui.password.setStyleSheet(DEFAULT_LINE_STYLE.replace(BLACK, RED))
+        elif len(login) != 0 and info[1]:
             with open("user_data/face_photo.png", "wb") as photo:
                 photo.write(info[2])
-            mtcnn = MTCNN(image_size=500, margin=0, min_face_size=200)  # initializing mtcnn for face detection
+            mtcnn = MTCNN(image_size=1000, margin=0, min_face_size=20)  # initializing mtcnn for face detection
             resnet = InceptionResnetV1(pretrained='vggface2').eval()
             face_database = mtcnn(Image.open("user_data/face_photo.png"))
             emb_database = resnet(face_database.unsqueeze(0)).detach()
-            cam = cv2.VideoCapture(0)
             count_trys = 0
             count_success = 0
             while count_trys < 2:
-                result, image = cam.read()
+                result, image = self.cam.read()
                 cv2.imwrite("user_data/face_photo.png", image)
                 face_current = mtcnn(Image.open("user_data/face_photo.png"))
-                emb_current = resnet(face_current.unsqueeze(0)).detach()
-                distance = dist(emb_current, emb_database).item()
-                print(distance)
-                if distance < 0.6:
-                    count_success += 1
+                if face_current is not None:
+                    emb_current = resnet(face_current.unsqueeze(0)).detach()
+                    distance = dist(emb_current, emb_database).item()
+                    print(distance)
+                    if distance < 0.6:
+                        count_success += 1
                 count_trys += 1
+            self.cam.stop()
             if count_success > 0:
+                with open("user_data/data.py", "w") as datafile:
+                    datafile.seek(0)
+                    datafile.write(f"logged = True\nlogin = '{login}'\npassword = '{info[0]}'\n")
+                    datafile.truncate()
                 self.close()
                 self.main = MainWindow(login)
                 self.main.show()
             else:
-                self.ui.helloTitle.setText("Error, try again or sign in with password")
-        elif len(login) != 0 and len(password) != 0:
-            try:
-                # connect to database
-                connection = self.connect_to_db()
-
-                with connection.cursor() as cursor:
-                    cursor.execute(f"SELECT password from users where login = '{login}'")
-                    user_password = cursor.fetchone()
-
-                    # if no user with such login
-                    if user_password is None:
-                        self.ui.helloTitle.setText("Wrong login!")
-                        self.ui.login.setStyleSheet(DEFAULT_LINE_STYLE.replace(BLACK, RED))
-                    # if database password == input password
-                    elif user_password[0] == password:
-                        with open("user_data/data.py", "w") as datafile:
-                            datafile.seek(0)
-                            datafile.write(f"logged = True\nlogin = '{login}'\npassword = '{password}'\n")
-                            datafile.truncate()
-
-                        self.ui.helloTitle.setText("PASSED!")
-                        self.ui.helloTitle.setStyleSheet("color: rgb(120, 183, 140);")
-                        self.close()
-                        self.main = MainWindow(login)
-                        self.main.show()
-                    # if database password != input password
-                    else:
-                        self.ui.helloTitle.setText("Wrong password!")
-                        self.ui.password.setStyleSheet(DEFAULT_LINE_STYLE.replace(BLACK, RED))
-                # close connection to database
-                connection.close()
-            except psycopg2.Error as _ex:
-                print("[INFO] database working error")
+                self.ui.helloTitle.setText("Error, try again or use your password")
 
     def openSignUp(self):
         self.close()
