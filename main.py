72dcb6f7b8c2ff93084046ad155cc8cfabf28820
MaffyMaths <matvey33.baryshev@mail.ru>
2023-05-09 12:53:08 +0300

    fix taskbar icon display

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Application initializing file.
 """
 
 import sys
+import ctypes
 from PyQt6.QtWidgets import QApplication
 from windows.sign_in_window import SignIn   # sign in window
 from windows.main_window import MainWindow  # main window
@@ -20,16 +21,19 @@ def main():
 
     :return: Zero on successful program and app finishing
     """
+    app_id = 'shmatozz.cw.faceauth.1'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)  # set custom app id
+
     app = QApplication(sys.argv)  # init app
 
     # if logged flag -> pass logged user to main menu
     if logged and len(login) > 0:
-        main_menu = MainWindow(login)
-        main_menu.show()
+        window = MainWindow(login)
+        window.show()
     # if no logged flag -> open sign in window
     else:
-        sign_in_menu = SignIn()
-        sign_in_menu.show()
+        window = SignIn()
+        window.show()
 
     app.exec()  # close app
 
